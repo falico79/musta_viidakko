ec55cc00f7bb24a2d9ed0c0718f08940f26e4e27
t6mymi00 <t6mymi00@students.oamk.fi>
2018-03-27 13:56:12 +0300

    päivitetty banaanipaneeli ja health v0.1

--- app/src/main/java/com/leaves/nine/mustamets/UserInterface.java
+++ app/src/main/java/com/leaves/nine/mustamets/UserInterface.java
@@ -18,8 +18,8 @@ public class UserInterface implements GameObject{
 
     Bitmap bananaCountIcon;
 
-    private static int bananas = 0;
-
+    private static int bananas = 999;
+    private static int health = 50;
 
     public UserInterface() {
         rectPanel = new Rect(0,0,Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT / 10);
@@ -35,8 +35,12 @@ public class UserInterface implements GameObject{
     }
 
     public static void removeBanana() {
-        if (bananas > 0)
+        if (bananas > 0 && health <= 95) {
             bananas--;
+            health += 5;
+        }
+
+
     }
 
     public static int getBananas() {
@@ -49,10 +53,12 @@ public class UserInterface implements GameObject{
             paint.setTextSize(75);
             paint.setColor(Color.YELLOW);
         drawBananaCount(canvas, paint, "x " + bananas);
+        paint.setColor(Color.RED);
+        drawHealth(canvas, paint, "Health: " + health);
     }
 
     public boolean playerCollide(Rect rect) {
-        return Rect.intersects(target, rect);// || Rect.intersects(rectangle2, player.getRectangle());
+        return Rect.intersects(new Rect(target.left - 15, target.top - 15, target.right + 125, target.bottom + 15), rect);// || Rect.intersects(rectangle2, player.getRectangle());
     }
 
     private void drawBananaCount(Canvas canvas, Paint paint, String text) {
@@ -62,6 +68,19 @@ public class UserInterface implements GameObject{
         int cWidth = rectPanel.width();
         paint.getTextBounds(text, 0, text.length(), rectPanel);
         float x = Constants.SCREEN_WIDTH / 18;
+        float y = Constants.SCREEN_HEIGHT / 14;
+        canvas.drawText(text, x, y, paint);
+//        canvas.drawRect(target, paint);
+        canvas.drawBitmap(bananaCountIcon, source, target,null);
+    }
+
+    private void drawHealth(Canvas canvas, Paint paint, String text) {
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.getClipBounds(rectPanel);
+        int cHeight = rectPanel.height();
+        int cWidth = rectPanel.width();
+        paint.getTextBounds(text, 0, text.length(), rectPanel);
+        float x = Constants.SCREEN_WIDTH / 2;
         float y = Constants.SCREEN_HEIGHT / 14;
         canvas.drawText(text, x, y, paint);
 //        canvas.drawRect(target, paint);
