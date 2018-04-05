b68c5f9c1c973d117cfeffba32171be752c74af7
t6mymi00 <t6mymi00@students.oamk.fi>
2018-04-05 10:04:35 +0300

    popup message vain jos storyitem keräämättä uudet sahanteräkuvat

--- app/src/main/java/com/leaves/nine/mustamets/StoryItem.java
+++ app/src/main/java/com/leaves/nine/mustamets/StoryItem.java
@@ -1,6 +1,8 @@
 package com.leaves.nine.mustamets;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 
 import java.util.Random;
@@ -15,10 +17,14 @@ public class StoryItem extends Collectible implements StoryObject {
     private long targetTime;
 
     private boolean completed = false;
-
+    private Canvas myCanvas;
 
     public StoryItem(AnimationManager animationManager, Rect target) {
         super(target, animationManager);
+    }
+
+    public StoryItem(){
+
     }
 
     @Override
@@ -30,7 +36,6 @@ public class StoryItem extends Collectible implements StoryObject {
             state = 0;
             animationManager.playAnim(state);
             targetTime = System.currentTimeMillis() + 5000 + new Random().nextInt(20000);
-
         }
         if(state == 0){
             if(System.currentTimeMillis()>= targetTime) {
@@ -49,13 +54,19 @@ public class StoryItem extends Collectible implements StoryObject {
         super.draw(canvas);
 
         //animationManager.draw(canvas, target);
-
-
+        openStoryBoard();
+        myCanvas = canvas;
     }
 
     @Override
     public void openStoryBoard() {
-
+        if (DoorObject.drawPopup && !completed) {
+            Paint paint = new Paint();
+            paint.setColor(Color.GREEN);
+            paint.setTextAlign(Paint.Align.CENTER);
+            paint.setTextSize((Constants.SCREEN_HEIGHT / 15));
+            DoorObject.drawPopupMessage(myCanvas, paint, "Kerää sahanterä ensin!!!");
+        }
     }
 
     @Override
